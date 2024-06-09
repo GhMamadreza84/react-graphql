43288ab7995133dfc12636dbb5a89ca9d0ec5b42
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-09 12:25:10 -0700

    get data by id and show in ui with graphql

--- src/components/User.js
+++ src/components/User.js
@@ -18,12 +18,20 @@ function User() {
   const changeHandler = (e) => {
     setId(e.target.value);
   };
-console.log({ loading, data, error });
+  console.log({ loading, data, error });
 
   return (
     <div>
       <input value={id} onChange={changeHandler} />
-      {/* <h3>{data.user.name}</h3> */}
+      {data ? (
+        <>
+          <h1>{data.user.name}</h1>
+          <p>{data.user.email}</p>
+          <p>{data.user.phone}</p>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
