20e8d537b0fba3e8fa975044b155d41355800149
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-09 23:32:53 -0700

    get user with lazyquery hook

--- src/components/UserOnClick.js
+++ src/components/UserOnClick.js
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { gql, useLazyQuery} from "@apollo/client";
+import { GET_USER } from "../graphql/queries";
 function UserOnClick() {
+  const [id, setId] = useState(1);
+  const response = useLazyQuery(GET_USER, {
+    variables: { id: id },
+  });
+  const changeHandler = (e) => {
+    setId(e.target.value);
+  };
+  console.log({ loading, data, error });
+
   return (
     <div>
-      
+      <input value={id} onChange={changeHandler} />
+      <button >search user</button>
+      {/* {data ? (
+        <>
+          <h1>{data.user.name}</h1>
+          <p>{data.user.email}</p>
+          <p>{data.user.phone}</p>
+        </>
+      ) : (
+        ""
+      )} */}
     </div>
-  )
+  );
 }
 
-export default UserOnClick
+export default UserOnClick;
