54f26c141e0376059623802038ea3af13e26f499
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-10 00:25:48 -0700

    add variables to mutation for create user

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { gql, useMutation } from "@apollo/client";
 const CREATE_USER = gql`
-  mutation {
+  mutation createUser(
+    $name: String!
+    $username: String!
+    $email: String!
+    $phone: String!
+  ) {
     createUser(
       input: {
-        name: "MMd"
-        username: "gh"
-        email: "gh@gmail.com"
-        phone: "090125214"
+        name: $name
+        username: $username
+        email: $email
+        phone: $phone
       }
     ) {
       id
@@ -19,8 +24,17 @@ const CREATE_USER = gql`
   }
 `;
 function CreateUser() {
-  const [createUser, { loading, data, error, called }] =
-    useMutation(CREATE_USER);
+  const [createUser, { loading, data, error, called }] = useMutation(
+    CREATE_USER,
+    {
+      variables: {
+        name: "MMd",
+        username: "gh",
+        email: "gh@gmail.com",
+        phone: "090125214",
+      },
+    }
+  );
 
   console.log(createUser, { loading, data, error, called });
   return (
