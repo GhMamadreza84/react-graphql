c67641ae8b77f2ac573d138b96ad1f8965184888
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-10 02:26:01 -0700

    create update_user in component

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import { gql,useMutation } from '@apollo/client'
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
 function UpdateUser() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const UPDATE_USER = gql`
+    mutation {
+      updateUser(id: 5, input: { username: "gh", email: "gh@gmail.com" }) {
+        id
+        name
+        username
+        email
+      }
+    }
+  `;
+  return <div></div>;
 }
 
-export default UpdateUser
+export default UpdateUser;
